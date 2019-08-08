7bca5b25a14c20515ba7ccee382493a444a0de76
liangjia <liangjia@variflight.com>
2019-08-08 19:05:04 +0800

    1.modify 匹配规则可配置

--- magic_time.py
+++ magic_time.py
@@ -3,61 +3,51 @@ import sublime_plugin
 import time
 import re
 
-class MagicTimeCommand(sublime_plugin.TextCommand):
+class MagicTimeEncodeCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
 		for s in self.view.sel():
 			if s.empty() or s.size() <= 1:
 				break
-			strs = self.view.substr(s)
+			
+			select_strs = self.view.substr(s)
+			settings    = sublime.load_settings('MagicTime.sublime-settings')
+			encode_rule = settings.get('encode_rule')
 
-			type1 = re.compile(r'^[0-9]+$')
-			type2 = re.compile(r'^([1-9][0-9][0-9][0-9])-(0?[1-9]|1[0-2])-(0?[1-9]|1[0-9]|2[0-9]|3[0-1])$')
-			type3 = re.compile(r'^([1-9][0-9][0-9][0-9])/(0?[1-9]|1[0-2])/(0?[1-9]|1[0-9]|2[0-9]|3[0-1])$')
-			type4 = re.compile(r'^([1-9][0-9][0-9][0-9])年(0?[1-9]|1[0-2])月(0?[1-9]|1[0-9]|2[0-9]|3[0-1])$')
-			type5 = re.compile(r'^([1-9][0-9][0-9][0-9])年(0?[1-9]|1[0-2])月(0?[1-9]|1[0-9]|2[0-9]|3[0-1])日$')
-			type6 = re.compile(r'^([1-9][0-9][0-9][0-9])-(0?[1-9]|1[0-2])-(0?[1-9]|1[0-9]|2[0-9]|3[0-1]) (0?[0-9]|1[0-9]|2[0-3]):(0?[0-9]|[1-5][0-9]):(0?[0-9]|[1-5][0-9])$')
-			type7 = re.compile(r'^([1-9][0-9][0-9][0-9])/(0?[1-9]|1[0-2])/(0?[1-9]|1[0-9]|2[0-9]|3[0-1]) (0?[0-9]|1[0-9]|2[0-3]):(0?[0-9]|[1-5][0-9]):(0?[0-9]|[1-5][0-9])$')
-			type8 = re.compile(r'^([1-9][0-9][0-9][0-9])年(0?[1-9]|1[0-2])月(0?[1-9]|1[0-9]|2[0-9]|3[0-1]) (0?[0-9]|1[0-9]|2[0-3]):(0?[0-9]|[1-5][0-9]):(0?[0-9]|[1-5][0-9])$')
-			type9 = re.compile(r'^([1-9][0-9][0-9][0-9])年(0?[1-9]|1[0-2])月(0?[1-9]|1[0-9]|2[0-9]|3[0-1])日 (0?[0-9]|1[0-9]|2[0-3]):(0?[0-9]|[1-5][0-9]):(0?[0-9]|[1-5][0-9])$')
+			for rule in encode_rule:
+				if re.compile(r'' + rule['regexp'] + '').match(select_strs):
+					
+					struct_time = time.strptime(select_strs, rule['format'])
+					content     = str(int(time.mktime(struct_time)))
+					
+					self.view.run_command('cut')
+					self.view.insert(edit, s.begin(), content)
 
-			if type1.match(strs):
-				ltime   = time.localtime(int(strs))
-				content = time.strftime("%Y-%m-%d %H:%M:%S", ltime)
-			elif type2.match(strs):
-				timeArray = time.strptime(strs, "%Y-%m-%d")
-				content   = str(int(time.mktime(timeArray)))
-			elif type3.match(strs):
-				timeArray = time.strptime(strs, "%Y/%m/%d")
-				content   = str(int(time.mktime(timeArray)))
-			elif type4.match(strs):
-				timeArray = time.strptime(strs, "%Y年%m月%d")
-				content   = str(int(time.mktime(timeArray)))
-			elif type5.match(strs):
-				timeArray = time.strptime(strs, "%Y年%m月%d日")
-				content   = str(int(time.mktime(timeArray)))
-			elif type6.match(strs):
-				timeArray = time.strptime(strs, "%Y-%m-%d %H:%M:%S")
-				content   = str(int(time.mktime(timeArray)))
-			elif type7.match(strs):
-				timeArray = time.strptime(strs, "%Y/%m/%d %H:%M:%S")
-				content   = str(int(time.mktime(timeArray)))
-			elif type8.match(strs):
-				timeArray = time.strptime(strs, "%Y年%m月%d %H:%M:%S")
-				content   = str(int(time.mktime(timeArray)))
-			elif type9.match(strs):
-				timeArray = time.strptime(strs, "%Y年%m月%d日 %H:%M:%S")
-				content   = str(int(time.mktime(timeArray)))
-			else:
-				break
+					break
 
-			self.view.run_command('cut')
-			self.view.insert(edit, s.begin(), content)
-
-class MagicNewTimeCommand(sublime_plugin.TextCommand):
+class MagicTimeDecodeCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
 		for s in self.view.sel():
-			content = str(int(time.time()))
+			if s.empty() or s.size() <= 1:
+				break
+			
+			select_strs = self.view.substr(s)
+			settings    = sublime.load_settings('MagicTime.sublime-settings')
+			decode_rule = settings.get('decode_rule')
+
+			for rule in decode_rule:
+				if re.compile(r'' + rule['regexp'] + '').match(select_strs):
+					
+					struct_time = time.localtime(int(select_strs))
+					content     = time.strftime(rule['format'], struct_time)
+
+					self.view.run_command('cut')
+					self.view.insert(edit, s.begin(), content)
+					break
+
+class MagicTimeCreateCommand(sublime_plugin.TextCommand):
+	def run(self, edit):
+		for s in self.view.sel():
 			if not s.empty() or s.size() > 1:
 				self.view.run_command('cut')
 
-			self.view.insert(edit, s.begin(), content)
+			self.view.insert(edit, s.begin(), str(int(time.time())))
